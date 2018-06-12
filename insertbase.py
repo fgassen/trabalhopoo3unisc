d0b9a2d4ebd1945a51ef5d6b60e393539ea8d9c1
Rodrigo Alves <rodrigo.a.da.silveira@gmail.com>
2018-06-12 19:59:23 -0300

    Update insertbase.py

--- insertbase.py
+++ insertbase.py
@@ -5,13 +5,16 @@ conteudo = arquivo.readlines()
 
 insert  = "INSERT INTO `receitas` (`id`, `receita`, `fr`, `descricao`, `orcada`, `atualizada`, `arrecada_mes`, `arrecada_exercicio`) VALUES (NULL"
 
-,, , , , , , );
-
-
 for linha in conteudo:
 	
 	dado = linha.split(',')
-	 insert = insert+
+	 insert = insert+dado[0]+
+		","+dado[1]+
+		","+dado[2]+
+		","+dado[3]+"."+dado[4]+
+		","+dado[5]+"."+dado[6]+
+		","+dado[7]+"."+dado[8]+
+		","+dado[9]+"."+dado[10]+");"
 	
 
 	print(dado)
