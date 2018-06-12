863e192097b6f7fe27073df4c5567c8bdbc4ae52
Rodrigo Alves <rodrigo.a.da.silveira@gmail.com>
2018-06-12 19:38:42 -0300

    Update insertbase.py

--- insertbase.py
+++ insertbase.py
@@ -3,11 +3,18 @@ arquivo = open('BalanceteReceitaOrinal.txt', 'r')
 
 conteudo = arquivo.readlines()
 
-arquivo.close()
+insert  = "INSERT INTO `receitas` (`id`, `receita`, `fr`, `descricao`, `orcada`, `atualizada`, `arrecada_mes`, `arrecada_exercicio`) VALUES (NULL"
+
+,, , , , , , );
+
 
 for linha in conteudo:
+	
 	dado = linha.split(',')
+	 insert = insert+
+	
+
 	print(dado)
 	
-	
+arquivo.close()
 
